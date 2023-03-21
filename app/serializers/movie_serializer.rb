55323eefbe4f163d0bcd441e93ed0e001d562c00
tony555t <tonytullah764@gmail.com>
2023-03-21 14:31:50 +0300

    model serializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -2,6 +2,6 @@ class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director, :summary
 
   def summary
-    
+    "#{self.object.title} - #{self.object.description[0..49]}..."
   end
 end
